57ea95d28ca35d72a58c3423e60a68f25ff2b566
IvanDenkov <denkov_v@ukr.net>
2021-10-14 18:06:50 +0300

    hw-2

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const num1 = +prompt("Введите первое число");
+const num2 = +prompt("Введите второе число");
+const add = num1 + num2;
+const sub = num1 * num2;
+const multi = num1 / num2;
+const dev = num1 / num2;
+console.log(add,sub,multi,dev);
